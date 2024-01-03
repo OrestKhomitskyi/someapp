6449970503ad458cb01cce411e424ba8c9e52eaf
Orest Khomitskyi <okhomitskyi@vodworks.com>
2024-01-03 14:25:01 +0200

    f

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactdd
+          Learn Reactddg
         </a>
       </header>
     </div>
